b73f6231f2ebaead2591af7ba084f6ed8ff657c0
Nicola Puddu <n.puddu@entando.com>
2018-12-17 11:27:43 +0100

    switched to locale

--- packages/entando-utils/modules/dates.js
+++ packages/entando-utils/modules/dates.js
@@ -1,6 +1,9 @@
-import moment from 'moment';
-
 // eslint-disable-next-line import/prefer-default-export
-export const getMonthDayYearFormat = (date, locale = 'en-us') => (
-  moment(date).locale(locale).format('MMMM, D, YYYY')
-);
+export const getMonthDayYearFormat = (date, locale = 'en-us') => {
+  const dateObject = new Date(date);
+  return dateObject.toLocaleString(locale, {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+};
