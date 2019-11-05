aea71fd1685bd535ebb5f26053bd2822f71a9655
Giuseppe de Santis <gdesantis@gmail.com>
2019-11-05 10:54:25 +0100

    EN6-134 login screen update

--- packages/entando-pages/modules/login/LoginPage.js
+++ packages/entando-pages/modules/login/LoginPage.js
@@ -6,7 +6,7 @@ import { Col } from 'patternfly-react';
 const LOGINPAGE_TITLE = 'fl.pages.loginPage.title';
 const LOGINPAGE_SUBTITLE = 'fl.pages.loginPage.subtitle';
 const LOGINPAGE_ABSTRACT = 'fl.pages.loginPage.abstract';
-const LOGINPAGE_COPYWRIGHT = 'fl.pages.loginPage.copywright';
+const LOGINPAGE_COPYRIGHT = 'fl.pages.loginPage.copyright';
 const LOGINPAGE_ENTANDO = 'fl.pages.loginPage.entando';
 
 const background = {
@@ -27,27 +27,24 @@ const LoginPage = ({ children }) => (
             <img src="images/entando-logo.svg" alt="" />
           </div>
           <p className="LoginPage__title">
-            <FormattedMessage id={LOGINPAGE_TITLE} defaultMessage="the leading platform" />
+            <FormattedMessage id={LOGINPAGE_TITLE} />
           </p>
           <p className="LoginPage__subtitle">
-            <FormattedMessage id={LOGINPAGE_SUBTITLE} defaultMessage="for cloud native applications" />
+            <FormattedMessage id={LOGINPAGE_SUBTITLE} />
           </p>
           <div className="LoginPage__spacer" />
           <p className="LoginPage__abstract">
             <FormattedMessage
               id={LOGINPAGE_ABSTRACT}
-              defaultMessage="Entando is the lightest open source Digital Transformation Platform for Modern Applications. We help customers bring to production enterprise applications that are lightweight, cloud native, and highly customized, and do so significantly faster than their competitors. Learn quickly, develop easily, deploy rapidly."
             />
           </p>
-          <div className="LoginPage__copywright" >
+          <div className="LoginPage__copyright" >
             <FormattedMessage
-              id={LOGINPAGE_COPYWRIGHT}
-              defaultMessage="Copyright 2019 Entando"
+              id={LOGINPAGE_COPYRIGHT}
             />
             <span className="LoginPage__entando">&nbsp;
               <FormattedMessage
                 id={LOGINPAGE_ENTANDO}
-                defaultMessage="Entando"
               />
             </span>
           </div>
