778f58d56ad4d452625f43703124950d4bf53e5f
Irakli Chalagashvili <chalagashvili5@gmail.com>
2022-09-27 16:18:06 +0400

    ENG-4121 replace internal server error texts

--- packages/entando-apimanager/modules/locales/en.js
+++ packages/entando-apimanager/modules/locales/en.js
@@ -1,8 +1,8 @@
 export default {
   locale: 'en',
   messages: {
-    'app.serverError': 'Could not establish connection with {domain}',
-    'app.noJsonReturned': 'Could not establish connection with {domain}',
+    'app.serverError': 'a generic error has occurred on the server',
+    'app.noJsonReturned': 'a generic error has occurred on the server',
     'app.permissionDenied': 'Your session expired',
     'app.serviceUnavailable': 'Server is unavailable',
     'app.badRequest': 'Bad Request',
--- packages/entando-apimanager/modules/locales/it.js
+++ packages/entando-apimanager/modules/locales/it.js
@@ -1,8 +1,8 @@
 export default {
   locale: 'it',
   messages: {
-    'app.serverError': 'Non è possibile raggiungere {domain}',
-    'app.noJsonReturned': 'Non è possibile raggiungere {domain}',
+    'app.serverError': 'si è verificato un errore generico lato server',
+    'app.noJsonReturned': 'si è verificato un errore generico lato server',
     'app.permissionDenied': 'Sessione scaduta',
     'app.serviceUnavailable': 'Il server non è disponibile',
     'app.badRequest': 'Richiesta Errata',
